e39c8fd3d7b8f083432bc74f081b15bcb39bca51
psawa <38402230+psawa@users.noreply.github.com>
2019-04-15 18:03:21 +0200

    Projet fini
    Sauf si on veut rajouter des détails, mais ya déjà beaucoup de détails.
    
    Et je crois que le bug s'est arrêté

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let mainCarre = document.querySelector('#mainCarre');
 let playButton = document.querySelector('#playButton');
 let homeContent = document.querySelector('#home-content');
 let bpmAccueil = 130;
-let menuAudio = new Audio('audio/menu.mp3')
+let menuAudio = new Audio('audio/colorrush2.mp3')
 menuAudio.play();
 let selectMode = document.querySelector('#modeSelection')
 
@@ -40,24 +40,32 @@ roundAudios[6] = new Audio('audio/round7.mp3');
 roundAudios[7] = new Audio('audio/round8.mp3');
 roundAudios[8] = new Audio('audio/round9.mp3');
 roundAudios[9] = new Audio('audio/finalRound.mp3');
+//Musiques de playlist
+let music1 = [new Audio('audio/music1.mp3'),140,18.2]; //file, bpm, drop-timecode
+let music2 = [new Audio('audio/music2.mp3'),163.5,23];
+let music3 = [new Audio('audio/music3.mp3'),120,4];
+let music4 = [new Audio('audio/music4.mp3'),120,15.1];
+let music5 = [new Audio('audio/music5.mp3'),143,14.3];
+let music6 = [new Audio('audio/music6.mp3'),152,18.3];
+let music7 = [new Audio('audio/music7.mp3'),133,28];
+
+let testAudio = [new Audio('audio/test.mp3'),200,"dropTimeCode"];
+
 
 
 let mySquareElement = document.querySelector('#mySquare');
 let targetSquareElement = document.querySelector('#targetSquare');
 let gameContent = document.querySelector('#game-content');
-let gameAudio = new Audio('audio/game.mp3');
 let squaresZone = document.querySelector('#squares-zone');
 let checkButton = document.querySelector('#checkButton');
 let slidersZone = document.querySelector('#sliders-zone')
 let menuOrRetryZone = document.querySelector('#menu-or-retry-zone');
 let playAgainButton = document.querySelector('#play-again-button');
 let goToMenuButton = document.querySelector('#go-to-menu-button');
-let slider1 = document.querySelector('#slider1'); //R if Rgb, H if HSL...
-let valeur1 = document.querySelector('#valeur1');
-let slider2 = document.querySelector('#slider2'); //G if RGb, S if HSL..
-let valeur2 = document.querySelector('#valeur2');
-let slider3 = document.querySelector('#slider3'); //B if RGB, L if HSL..
-let valeur3 = document.querySelector('#valeur3');
+
+var valeur1 = '';
+var valeur2 = '';
+var valeur3 = '';
 
 //Élements persistents
 let container = document.querySelector('#container')
@@ -100,14 +108,15 @@ playAgainButton.addEventListener('click', function() {
 });
 goToMenuButton.addEventListener('click', function() {
   game.goToMenu()
+  affichage.deleteSliders();
 });
 
 
 
 //Jeu
-slider1.addEventListener('input', change1);
-slider2.addEventListener('input', change2);
-slider3.addEventListener('input', change3);
+// slider1.addEventListener('input', change1);
+// slider2.addEventListener('input', change2);
+// slider3.addEventListener('input', change3);
 //slider4.addEventListener('input', change4);
 
 /*
@@ -117,28 +126,37 @@ slider3.addEventListener('input', change3);
 */
 
 function change1(event) {
-  if (game.mode === 'rgb') {
     valeur1.innerHTML = event.target.value;
-  }
   updateMySquareColor();
 }
 
 function change2(event) {
-  if (game.mode === 'rgb') {
     valeur2.innerHTML = event.target.value;
-  }
   updateMySquareColor();
 }
 
 function change3(event) {
-  if (game.mode === 'rgb') {
     valeur3.innerHTML = event.target.value;
-  }
   updateMySquareColor();
 }
 
 function updateMySquareColor() {
-  mySquare.color = `rgb(${valeur1.innerHTML},${valeur2.innerHTML},${valeur3.innerHTML})`;
+  if (game.mode ==='rgb') {
+      mySquare.color = `rgb(${valeur1.innerHTML},${valeur2.innerHTML},${valeur3.innerHTML})`;
+  }
+  else if(game.mode==="hsl"){
+    mySquare.color = `hsl(${valeur1.innerHTML},${valeur2.innerHTML}%,${valeur3.innerHTML}%)`;
+  }
+  else if(game.mode==="cmy"){
+    let c = parseInt(valeur1.innerHTML);
+    let m = parseInt(valeur2.innerHTML);
+    let y = parseInt(valeur3.innerHTML);
+    let k = 0;
+
+    let code = CMYKtoRGB(c,m,y,k);
+    mySquare.color = `rgb(${code[0]},${code[1]},${code[2]})`;
+  }
+
 }
 
 function parseBpmIntoInterval(bpm) {
@@ -160,7 +178,7 @@ function animateMainCarre() {
     let randomB = randomInt(0, 255);
     //change le carré et le fond de couleur en rythme
     mainCarre.style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`;
-    container.style.background = `linear-gradient(rgba(${randomR},${randomG},${randomB},0.01),#00163a)`
+    container.style.background = `linear-gradient(rgba(${randomR},${randomG},${randomB},0.01),#00163a)`;
     mainCarre.style.transform = 'scale(1.02)';
     //Animation du carré qui grossit
     setTimeout(function() {
@@ -180,18 +198,37 @@ function parseIntoRgb(code, mode = game.mode) {
   //Convertit un code couleur en rgb
   //Puis renvoie les composantes
   //pour pouvoir effectuer le calcul de distance entre 2 couleurs
-  if (mode === "rgb") {
-    code = code.substr(4); //On extrait les caractères à partir du 4eme
-    code = code.substring(0, code.length - 1); //et on enlève le dernier
-    codeArray = code.split(','); //Et on transforme en tableau
-    return codeArray;
-  } else if (mode === "hsl") {
-    null
-  } else if (mode === "cmy") {
-    null
-  }
+  code = code.substr(4); //On extrait les caractères à partir du 4eme
+  code = code.substring(0, code.length - 1); //et on enlève le dernier
+  codeArray = code.split(','); //Et on transforme en tableau
+  return codeArray;
 }
+// function cmykToRgb(c, m, y, k) {
+//   var r, g, b;
+//   r = 255 - ((Math.min(1, c * (1 - k) + k)) * 255);
+//   g = 255 - ((Math.min(1, m * (1 - k) + k)) * 255);
+//   b = 255 - ((Math.min(1, y * (1 - k) + k)) * 255);
+//   return {r : r, g : g, b : b};
+// }
 
+function CMYKtoRGB(c,m,y,k){
+		var result = new Array();
+
+		c = c / 100;
+		m = m / 100;
+		y = y / 100;
+		k = k / 100;
+
+		result[0] = 1 - Math.min( 1, c * ( 1 - k ) + k );
+		result[1] = 1 - Math.min( 1, m * ( 1 - k ) + k );
+		result[2] = 1 - Math.min( 1, y * ( 1 - k ) + k );
+
+		result[0] = Math.round( result[0] * 255 );
+		result[1] = Math.round( result[1] * 255 );
+		result[2] = Math.round( result[2] * 255 );
+
+		return result;
+	}
 
 function rgb2lab(rgb) {
   var r = rgb[0] / 255,
@@ -260,6 +297,7 @@ let game = {
     //Quand le temps est imparti, ou quand il valide
     //appelle parseIntoRgb
     let distance = deltaE(targetComponentsLab, myComponentsLab).toFixed(2); //merci github
+    console.log(distance);
     this.currentDistance = distance;
     affichage.checking();
     setTimeout(function() {
@@ -273,18 +311,29 @@ let game = {
   nextLevel: function() {
     game.state = "playing";
     if (this.level === this.maxLevel) {
-      game.state = "win";
-      return null; // Affiche l'écran de gagné
+      this.state = "win";
+      this.gameWin();
+      // Affiche l'écran de gagné
     }
-    this.level += 1;
-    roundAudios[this.level-1].play();
-    document.querySelector('#current-level').innerHTML = this.level;
-    targetSquare.randomColor();
-    mySquare.color = 'rgb(255,255,255)';
-    timer.newTimer();
+    else{
+      this.level += 1;
+      affichage.clear();
+      roundAudios[this.level-1].play();
+      document.querySelector('#current-level').innerHTML = this.level;
+      targetSquare.randomColor();
+      if (game.mode==='cmy') {
+        mySquare.color = 'rgb(255,255,255)';
+      }
+      else{
+        mySquare.color = 'rgb(0,0,0)';
+
+      }
+      timer.newTimer();
+    }
   },
   lose: function() {
     //Quand on a perdu
+    this.state = "lose";
     affichage.newMessage('Maybe next time..');
     affichage.lose();
   },
@@ -295,19 +344,23 @@ let game = {
     } else if(game.currentDistance>10){
       affichage.newMessage('Nice !')
     } else if (game.currentDistance > 5) {
-      affichage.newMessage('OMG, YOU ROCK !');
+      affichage.newMessage('You rock !');
       groovyAudio.play();
     } else if (game.currentDistance > 3) {
-      affichage.newMessage('THAT\'S GREAT !');
+      affichage.newMessage('GREAT !');
       greatAudio.play();
     } else {
-      affichage.newMessage('NO FUCKING WAY !!!');
+      affichage.newMessage('PERFECT!');
       perfectAudio.play();
     }
     setTimeout(function() {
-      affichage.clear();
       game.nextLevel();
     }, 1000)
+  },
+  gameWin: function(){
+    this.state = "win";
+    congratulationsAudio.play();
+    affichage.gameWin();
 
   },
   start: function() {
@@ -316,7 +369,9 @@ let game = {
     this.level = 0;
     document.querySelector('#explications').style.display = 'none';
     gameContent.style.display='none';
-    gameAudio.play();
+    if (this.state ==="menu") {
+      affichage.newSliders();
+    }
     setTimeout(function() {
       affichage.newMessage('Ready ?')
     }, 100)
@@ -339,10 +394,14 @@ let game = {
       }
     }, 2400)
     setTimeout(function() {
+      if (game.state === "menu") {
+        playList.launch();
+      }
       affichage.newMessage('GO');
       game.nextLevel();
       gameContent.style.display = '';
       affichage.clear()
+
     }, 3500)
 
 
@@ -352,18 +411,18 @@ let game = {
   },
   goToMenu: function() {
     if (this.state != 'menu') {
-      gameAudio.pause();
-      gameAudio.currentTime = 0;
+      playList.quit();
       menuAudio.play();
       gameContent.style.display = 'none';
       homeContent.style.display = '';
       document.querySelector('#explications').style.display = ''
+      this.state = 'menu';
     }
   }
 }
 
 let timer = {
-  values: [30, 27, 25, 22, 18, 15, 13, 11, 10, 8],
+  values: [30, 27, 24, 20, 17, 14, 12,10, 8, 7],
   interval: null,
   get currentTime() {
     return this.values[game.level - 1];
@@ -392,6 +451,7 @@ let timer = {
 class Square {
   constructor(DomElement) {
     this.element = DomElement;
+    this.bounceIntervalVar = null;
   }
   set color(color) {
     //remplit d'une certaine couleur
@@ -400,21 +460,32 @@ class Square {
   get color() {
     return this.element.style.backgroundColor;
   }
-  bounce() {
-    //Animation, le carré bounce à partir d'un certain temps
-    setInterval(function() {
-      if (jeu.state === 'play') {
-        //Animation
-      }
-    }, parseBpmIntoInterval(bpmGame))
+  get bounceInterval(){
+    return parseBpmIntoInterval(playList.currentBpm);
   }
+  bounce() {
+    console.log('Bounce function launched');
+    this.bounceIntervalVar = setInterval(()=>{
+      if (game.state === 'playing' || game.state==='checking') {
+        this.element.style.transform = 'scale(1.02)';
+        //Animation du carré qui grossit
+        setTimeout(()=> {
+          this.element.style.transform = 'scale(1)';
+        },50)
+      }
+    },this.bounceInterval)
+  }
+
+  cancelBounce(){
+    clearInterval(this.bounceIntervalVar);
+  }
+
   randomColor() {
     let randomR = randomInt(0, 255);
     let randomG = randomInt(0, 255);
     let randomB = randomInt(0, 255);
     this.color = `rgb(${randomR},${randomG},${randomB})`;
   }
-
 }
 
 const mySquare = new Square(mySquareElement);
@@ -441,24 +512,121 @@ let affichage = {
   },
   newPercentage: function() {
     let newPercentageDiv = document.createElement('p');
-    let newPercentage = document.createTextNode(100-game.currentDistance + '%');
+    let percentage = 100-game.currentDistance;
+    console.log(game.currentDistance);
+    let newPercentage = document.createTextNode( percentage + '%');
     newPercentageDiv.appendChild(newPercentage);
     newPercentageDiv.classList.add('percentage');
     squaresZone.appendChild(newPercentageDiv);
     newPercentageDiv.style.marginLeft = `-${newPercentageDiv.offsetWidth/2}px`;
     newPercentageDiv.style.marginTop = `-${newPercentageDiv.offsetHeight/2}px`;
   },
+  deleteSliders: function(){
+    slidersZone.parentNode.removeChild(slidersZone);
+  },
   newSliders: function(mode = game.mode) {
     //Crée les sliders adapté au mode sélectionné
     //Appelée une fois au début de la partie
+    let slidersZoneTemp = document.createElement('div');
+    slidersZoneTemp.setAttribute('id','sliders-zone');
+
+    let divSlider1Temp = document.createElement('div');
+
+    let tag1Temp = document.createElement('i');
+    divSlider1Temp.appendChild(tag1Temp);
+
+    let slider1Temp = document.createElement('input');
+    slider1Temp.setAttribute('type','range');
+    slider1Temp.setAttribute('id','slider1');
+    slider1Temp.setAttribute('min','0');
+    slider1Temp.setAttribute('value','0')
+    divSlider1Temp.appendChild(slider1Temp);
+
+    let valeur1Temp = document.createElement('i');
+    valeur1Temp.appendChild(document.createTextNode('0'));
+    valeur1Temp.setAttribute('id','valeur1')
+    divSlider1Temp.appendChild(valeur1Temp);
+    slidersZoneTemp.appendChild(divSlider1Temp);
+    slider1Temp.addEventListener('input',change1)
+
+    slidersZoneTemp.appendChild(divSlider1Temp);
+
+    let divSlider2Temp = document.createElement('div');
+
+    let tag2Temp = document.createElement('i');
+    divSlider2Temp.appendChild(tag2Temp);
+
+    let slider2Temp = document.createElement('input');
+    slider2Temp.setAttribute('type','range');
+    slider2Temp.setAttribute('id','slider2');
+    slider2Temp.setAttribute('min','0');
+    slider2Temp.setAttribute('value','0')
+    divSlider2Temp.appendChild(slider2Temp);
+
+    let valeur2Temp = document.createElement('i');
+    valeur2Temp.appendChild(document.createTextNode('0'));
+    valeur2Temp.setAttribute('id','valeur2')
+    divSlider2Temp.appendChild(valeur2Temp);
+    slidersZoneTemp.appendChild(divSlider2Temp);
+    slider2Temp.addEventListener('input',change2)
+
+    slidersZoneTemp.appendChild(divSlider2Temp);
+
+    let divSlider3Temp = document.createElement('div');
+
+    let tag3Temp = document.createElement('i');
+    divSlider3Temp.appendChild(tag3Temp);
+
+    let slider3Temp = document.createElement('input');
+    slider3Temp.setAttribute('type','range');
+    slider3Temp.setAttribute('id','slider3');
+    slider3Temp.setAttribute('min','0');
+    slider3Temp.setAttribute('value','0')
+    divSlider3Temp.appendChild(slider3Temp);
+
+    let valeur3Temp = document.createElement('i');
+    valeur3Temp.appendChild(document.createTextNode('0'));
+    valeur3Temp.setAttribute('id','valeur3')
+    divSlider3Temp.appendChild(valeur3Temp);
+    slidersZoneTemp.appendChild(divSlider3Temp);
+    slider3Temp.addEventListener('input',change3)
+
+    slidersZoneTemp.appendChild(divSlider3Temp);
+
+    squaresZone.after(slidersZoneTemp);
+    valeur1 = document.querySelector('#valeur1');
+    valeur2 = document.querySelector('#valeur2');
+    valeur3 = document.querySelector('#valeur3');
+
+    slidersZone = document.querySelector('#sliders-zone');
+
     if (mode === "rgb") {
-      null;
-    } else if (mode === "rgba") {
-      null;
+      tag1Temp.appendChild(document.createTextNode('R'));
+      slider1Temp.setAttribute('max','255');
+      slider1Temp.classList.add('rgb');
+      tag2Temp.appendChild(document.createTextNode('G'));
+      slider2Temp.setAttribute('max','255');
+      slider2Temp.classList.add('rgb');
+      tag3Temp.appendChild(document.createTextNode('B'));
+      slider3Temp.setAttribute('max','255');
+      slider3Temp.classList.add('rgb');
     } else if (mode === "hsl") {
-      null;
-    } else if (mode === "cmj") {
-      null;
+      tag1Temp.appendChild(document.createTextNode('H'));
+      slider1Temp.setAttribute('max','360');
+      tag2Temp.appendChild(document.createTextNode('S'));
+      slider2Temp.setAttribute('max','100');
+      tag3Temp.appendChild(document.createTextNode('L'));
+      slider3Temp.setAttribute('max','100');
+    } else if (mode === "cmy") {
+      tag1Temp.appendChild(document.createTextNode('C'));
+      slider1Temp.setAttribute('max','100');
+      slider1Temp.classList.add('cmy');
+      tag2Temp.appendChild(document.createTextNode('M'));
+      slider2Temp.setAttribute('max','100');
+      slider2Temp.classList.add('cmy');
+      tag3Temp.appendChild(document.createTextNode('Y'));
+      slider3Temp.setAttribute('max','100');
+      slider3Temp.classList.add('cmy');
     }
   },
   lose: function() {
@@ -493,13 +661,38 @@ let affichage = {
       newMessageDiv.parentNode.removeChild(newMessageDiv);
     }, 1400)
   },
+
+  gameWin:function(){
+    confettiRain();
+    playList.quit();
+    // currentAudio.pause()
+    gameWinAudio.play();
+    document.querySelector('#cibleTag').style.visibility = 'hidden';
+    document.querySelector('#monCarreTag').style.visibility = 'hidden';
+    document.querySelector('#my-square-zone').style.transform = 'translateX(-150px)';
+    setTimeout(function() {
+      squaresZone.style.transform = 'translateY(500px)';
+      menuOrRetryZone.style.display = '';
+      menuOrRetryZone.style.marginLeft = `-${menuOrRetryZone.offsetWidth/2}px`;
+
+    }, 3000)
+    setTimeout(function() {
+      squaresZone.style.display = 'none';
+    }, 6000)
+
+    document.querySelector('#time-zone').style.visibility = 'hidden';
+    document.querySelector('#target-square-zone').style.transform = 'translateX(150px)';
+    targetSquare.color = '#f2f200';
+    mySquare.color = '#f2f200';
+  },
+
   clear: function() {
     if (document.querySelector('.percentage') != null) {
       document.querySelector('.percentage').parentNode.removeChild(document.querySelector('.percentage'));
     }
-    valeur1;innerHTML='255';
-    valeur2;innerHTML='255';
-    valeur3;innerHTML='255';
+    // valeur1;innerHTML='0';
+    // valeur2;innerHTML='0';
+    // valeur3;innerHTML='0';
     document.querySelector('#time-zone').style.visibility = '';
     squaresZone.style.transform = 'translateY(0)';
     squaresZone.style.display='';
@@ -508,15 +701,68 @@ let affichage = {
     document.querySelector('#my-square-zone').style.transform = 'translateX(0)';
     document.querySelector('#target-square-zone').style.transform = 'translateX(0)';
     menuOrRetryZone.style.display='none';
-    checkButton.style.visibility = '';
     slidersZone.style.transform = 'translateY(0px)';
     slidersZone.style.display = '';
-    slider1.value = '255';
-    valeur1.innerHTML = '255';
-    slider2.value = '255';
-    valeur2.innerHTML = '255';
-    slider3.value = '255';
-    valeur3.innerHTML = '255';
+    slider1.value = 0;
+    slider2.value = 0;
+    slider3.value = 0;
+    valeur1.innerHTML= 0;
+    valeur2.innerHTML= 0;
+    valeur3.innerHTML= 0;
+    checkButton.style.visibility = '';
 
+    //
+    if (game.state ==="win") {
+      cancelAnimationFrame(confettiRain);
+    }
+  }
+}
+
+
+let playList = {
+  tracks: [music1,music2,music3,music5,music6,music7],
+  currentTrackNumber:0,
+  currentDropTimeOut:null,
+  get currentTrack(){
+    return this.tracks[this.currentTrackNumber];
+  },
+  get currentBpm(){
+    return this.currentTrack[1];
+  },
+  launch:function(){
+    for (let i = 0; i < this.tracks.length; i++) {
+      this.tracks[i][0].addEventListener('ended',()=>{
+        this.nextTrack();
+      });
+    }
+    this.playCurrentTrack();
+  },
+  nextTrack: function(){
+    mySquare.cancelBounce();
+    targetSquare.cancelBounce();
+    clearTimeout(this.currentDropTimeOut);
+    this.currentTrack[0].currentTime = 0;
+    this.currentTrack[0].pause();
+    if (this.currentTrackNumber === this.tracks.length-1) {
+      this.currentTrackNumber = 0
+    }
+    else{
+      this.currentTrackNumber +=1;
+    }
+    this.playCurrentTrack();
+  },
+  quit:function(){
+    this.currentTrack[0].currentTime = 0;
+    this.currentTrack[0].pause()
+    this.currentTrackNumber = 0
+  }
+  ,
+  playCurrentTrack: function(){
+    console.log('Playcurrent track appelé');
+    this.currentTrack[0].play();
+    this.currentDropTimeOut = setTimeout(()=>{
+      mySquare.bounce();
+      targetSquare.bounce();
+    },this.currentTrack[2]*1000)
   }
 }
